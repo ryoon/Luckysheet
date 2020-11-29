0de631554b0f95bd155413ce1d9f1712162b92e4
Ryo ONODERA <ryo@tetera.org>
2020-11-29 15:59:33 +0900

    Add Japanese locale skelton copied from English

--- src/locale/locale.js
+++ src/locale/locale.js
@@ -1,12 +1,14 @@
 import en from './en'
 import zh from './zh'
 import es from './es'
+import ja from './ja'
 import Store from '../store';
 
 const localeObj = {
     'en':en,
     'zh':zh,
-    'es':es
+    'es':es,
+    'ja':ja
 }
 
 function locale(){
